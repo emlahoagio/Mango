e46a65d0f0fbbd0d8cd6dd4febcd1f41b1992998
quantmdo <quandtmdo-kms@prxcision.com>
2023-11-09 23:25:51 +0700

    Mango.Web | build product detail UI

--- Mango.Web/Controllers/HomeController.cs
+++ Mango.Web/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Mango.Web.Models;
 using Newtonsoft.Json;
 using Mango.Web.Service.IService;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Mango.Web.Controllers;
 
@@ -30,6 +31,22 @@ public class HomeController : Controller
             TempData["error"] = response.Message;
         }
         return View(list);
+    }
+
+    [Authorize]
+    public async Task<IActionResult> ProductDetails(int productId)
+    {
+        ProductDto dto = new();
+        ResponseDto? response = await _productService.GetProductByIdAsync(productId);
+        if (response != null && response.IsSuccess)
+        {
+            dto = JsonConvert.DeserializeObject<ProductDto>(Convert.ToString(response.Result));
+        }
+        else
+        {
+            TempData["error"] = response.Message;
+        }
+        return View(dto);
     }
 
     public IActionResult Privacy()
