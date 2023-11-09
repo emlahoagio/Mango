ec4bf6ec7db04183566b28e6c1057cc4dd0b1f99
quantmdo <quandtmdo-kms@prxcision.com>
2023-11-09 23:18:12 +0700

    Mango.Web | build home page UI

--- Mango.Web/Controllers/HomeController.cs
+++ Mango.Web/Controllers/HomeController.cs
@@ -1,21 +1,35 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Mango.Web.Models;
+using Newtonsoft.Json;
+using Mango.Web.Service.IService;
 
 namespace Mango.Web.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly IProductService _productService;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, IProductService productService)
     {
         _logger = logger;
+        _productService = productService;
     }
 
-    public IActionResult Index()
+    public async Task<IActionResult> Index()
     {
-        return View();
+        List<ProductDto>? list = new();
+        ResponseDto? response = await _productService.GetAllProductAsync();
+        if (response != null && response.IsSuccess)
+        {
+            list = JsonConvert.DeserializeObject<List<ProductDto>>(Convert.ToString(response.Result));
+        }
+        else
+        {
+            TempData["error"] = response.Message;
+        }
+        return View(list);
     }
 
     public IActionResult Privacy()
