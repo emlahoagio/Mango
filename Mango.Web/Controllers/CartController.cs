1e5737ed66203e4bdcab68f56c3d718b255e27bf
quantmdo <quandtmdo-kms@prxcision.com>
2023-11-16 22:19:36 +0700

    return view

--- Mango.Web/Controllers/CartController.cs
+++ Mango.Web/Controllers/CartController.cs
@@ -56,9 +56,11 @@ namespace Mango.Web.Controllers
                 var stripeResponse = await _orderService.CreateStripeSession(stripeRequestDto);
                 StripeRequestDto stripeResponseResult = JsonConvert.DeserializeObject<StripeRequestDto>(Convert.ToString(stripeResponse.Result));
                 Response.Headers.Add("Location", stripeResponseResult.StripeSessionUrl);
+
                 return new StatusCodeResult(303);
+
             }
-            return RedirectToAction(nameof(CartIndex));
+            return View();
         }
 
         [Authorize]
